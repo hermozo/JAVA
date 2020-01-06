0b3ea6c1b75804e0884d9b14272d160e94ab06ab
miguel angel huaycho <mickyhermozo72@gmail.com>
2020-01-05 22:35:50 -0400

    Primeros pasos con java

---
+++ primerosPasos/src/main/java/com/mycompany/primerospasos/Main.java
@@ -0,0 +1,30 @@
+package com.mycompany.primerospasos;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i);
+        }
+        System.out.println("");
+        int contador = 0;
+        while (contador <= 10) {
+            System.out.print(contador);
+            contador++;
+        }
+        System.out.println("");
+        int p = 10;
+        do {
+            System.out.print(p);
+            p--;
+        } while (p != 0);
+        System.out.println("");
+        String[] lista = {"Miguel", "Angel", "Huaycho", "Hermozo"};
+        for (String x : lista) {
+            System.out.print(x + " ");
+        }
+
+    }
+
+}
